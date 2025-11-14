41d544c5027454dcc9835b756701cdb43c840ddd
Bill Hettinger <bill.hettinger@gmail.com>
2025-11-14 16:11:40 -0500

    Initial Commit

---
+++ flying/flyingApp.swift
@@ -0,0 +1,17 @@
+//
+//  flyingApp.swift
+//  flying
+//
+//  Created by bill on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct flyingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flyingTests/flyingTests.swift
@@ -0,0 +1,17 @@
+//
+//  flyingTests.swift
+//  flyingTests
+//
+//  Created by bill on 11/14/25.
+//
+
+import Testing
+@testable import flying
+
+struct flyingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
